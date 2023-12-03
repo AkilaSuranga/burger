2256404ffa320423c860c13b192fb626ccedfa25
123Akila <89736578+123Akila@users.noreply.github.com>
2023-12-02 19:32:13 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var button=document.getElementBy("header.navbar");
+// Add a click event listener to the button
+button.addEventListener("Shop", function() {
+    // Change the button text when clicked;
+});
+
+var button=document.getElementById("header.navbar");
+button.addEventListener("Blog,function"({
+    
+}))
